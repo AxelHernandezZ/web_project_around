5478fadef6f3975f19e4e4f7e24acc8b0bbb9e9c
Axel Andres Hernandez Zavaleta <and.herzav20@gmail.com>
2026-04-04 23:03:30 -0600

    fix: Recolocar script al final para un correcto funcionamiento

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-let WindowEdit = document.querySelector(".");
+let editButton = document.querySelector(".profile__info-editbutton-edit");
+let form = document.querySelector(".form");
+let formCloseButton = document.querySelector(".form__close");
+
+function formClose() {
+  form.classList.remove("form--visible");
+}
+
+function formToggle() {
+  form.classList.add("form--visible");
+}
+
+editButton.addEventListener("click", formToggle);
+formCloseButton.addEventListener("click", formClose);
+
+// cerrar al iniciar
+formClose();
