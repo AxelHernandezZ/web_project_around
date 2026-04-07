720c1d9dfcbdf04691414453f84bafa84427365d
Axel Andres Hernandez Zavaleta <and.herzav20@gmail.com>
2026-04-06 23:18:50 -0600

    fix: Conservar propiedades de input al guardadar

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
+//Perfil boton editar
 let editButton = document.querySelector(".profile__info-editbutton-edit");
+//Formulario
 let form = document.querySelector(".form");
 let formCloseButton = document.querySelector(".form__close");
 let formSaveButton = document.querySelector(".form__save");
+//Entradas de formulario
+let nameInput = document.querySelector(".form__name");
+let aboutInput = document.querySelector(".form__about");
 
 function formClose() {
+  nameInput.value = "";
+  aboutInput.value = "";
   form.classList.remove("form--visible");
 }
 
@@ -12,15 +19,15 @@ function formToggle() {
 }
 
 function EditProfile() {
-  let nameInput = document.querySelector(".form__name");
-  let aboutInput = document.querySelector(".form__about");
-  let profileName = document.querySelector(".profile__info-name");
-  let profileAbout = document.querySelector(".profile__info-explorador");
+  let profileName = document.querySelector(".profile__info-name-text");
+  let profileAbout = document.querySelector(".profile__info-explorador-text");
 
   console.log(nameInput.value);
   console.log(aboutInput.value);
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
+
+  formClose();
 }
 
 editButton.addEventListener("click", formToggle);
