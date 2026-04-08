80aed9d279b44d63352faa1c568e0800696eab46
Axel Andres Hernandez Zavaleta <and.herzav20@gmail.com>
2026-04-07 23:37:11 -0600

    feat: Agregar botones reactivos y corregir errores menores

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 //Perfil boton editar
 let editButton = document.querySelector(".profile__info-editbutton-edit");
+//Perfil boton guardar
+let saveButton = document.querySelector(".form__save");
 //Formulario
 let form = document.querySelector(".form");
 let formCloseButton = document.querySelector(".form__close");
@@ -8,16 +10,19 @@ let formSaveButton = document.querySelector(".form__save");
 let nameInput = document.querySelector(".form__name");
 let aboutInput = document.querySelector(".form__about");
 
+//Funcion para cerrar formulario y limpiar campos
 function formClose() {
   nameInput.value = "";
   aboutInput.value = "";
   form.classList.remove("form--visible");
 }
 
+//funcion para mostrar formulario
 function formToggle() {
   form.classList.add("form--visible");
 }
 
+//funcion para editar perfil y cerrar formulario
 function EditProfile() {
   let profileName = document.querySelector(".profile__info-name-text");
   let profileAbout = document.querySelector(".profile__info-explorador-text");
@@ -30,8 +35,21 @@ function EditProfile() {
   formClose();
 }
 
+//funcion para activar boton guardar
+function saveForm() {
+  if (nameInput.value.trim() !== "" && aboutInput.value.trim() !== "") {
+    saveButton.classList.add("form__save-active");
+  } else {
+    saveButton.classList.remove("form__save-active");
+    return;
+  }
+}
+
+//eventos
 editButton.addEventListener("click", formToggle);
 formCloseButton.addEventListener("click", formClose);
+nameInput.addEventListener("input", saveForm);
+aboutInput.addEventListener("input", saveForm);
 formSaveButton.addEventListener("click", EditProfile);
 
 // cerrar al iniciar
